44a994190356d664252512e343b4456733517268
yarden <hazanyarden@icloud.com>
2019-07-31 10:48:47 +0300

    3107

--- task/settings.py
+++ task/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -25,7 +26,7 @@ SECRET_KEY = '20cugwm5tb6p7n#(=*28x54(1p#0gf)u^%)ajhu)1pw_+k=e1*'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost,'167.71.62.254']
 
 
 # Application definition
@@ -131,3 +132,4 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+django_heroku.settings(locals())
